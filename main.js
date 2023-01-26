17a8f1e2f3d926c4d56c87296d66f0fdaff700a2
ville <kujala.ville.vk@gmail.com>
2023-01-26 10:50:44 +0200

    made bingo work

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
 let bingoContainer = document.getElementById("bingo");
-let bingoCard = []
+let resetButton = document.getElementById("resetButton");
+
+let bingoSessionData = []
+let bingoElements = []
 
 async function main() {
     try{
-        bingoCard = JSON.parse(localStorage.getItem("bingo"));
+        bingoSessionData = JSON.parse(localStorage.getItem("bingo"));
+        if (bingoSessionData == null){
+            throw new Error("Storage is empty!")
+        }
+        for(let i = 0; i < 25; i++){
+            populateElement(i, bingoSessionData[i].text, bingoSessionData[i].ticked)
+        }
     }catch{
-        bingoCard = await bingoGenerator();
+        bingoSessionData = await bingoGenerator();
     }
+
+    resetButton.addEventListener("click", resetBingo)
 
     bingoRender();
 }
 
+function resetBingo(event){
+    localStorage.removeItem("bingo");
+    location.reload();
+}
+
+function populateElement(id, title, ticked){
+    bingoElements[id] = document.createElement("div");
+    bingoElements[id].className = "bingoTile";
+    bingoElements[id].id = id;
+    bingoElements[id].innerHTML = title;
+    bingoElements[id].addEventListener("click", bingoClick);
+    if(ticked){
+        bingoElements[id].classList.add("ticked");
+    }
+}
+
 async function bingoGenerator() {
     let bingoCard = [];
+    let bingoElements = []
 
     let bingotiles = await (await fetch('bingo/bingotiles.json')).json();
     for (let i = 0; i < 25; i++) {
         let tilenumber = getRandomInt(0, bingotiles.length);
         bingoCard[i] = {
-            "element": document.createElement("div"),
             "text": await bingotiles[tilenumber],
             "ticked": false
         }
-        bingoCard[i].element.className = "bingoTile";
-        bingoCard[i].element.id = i;
-        bingoCard[i].element.innerHTML = bingotiles[tilenumber];
-        bingoCard[i].element.addEventListener("click", bingoClick)
+        populateElement(i, bingotiles[tilenumber], false);
         bingotiles.splice(tilenumber, 1);
     }
-    return bingoCard;
+    return bingoCard
 }
 
 function getRandomInt(min, max) {
@@ -39,28 +63,28 @@ function getRandomInt(min, max) {
 
 function bingoClick(event) {
     let targetid = event.target.id;
-    console.log(bingoCard[targetid].ticked)
+    console.log(bingoSessionData[targetid])
 
-    if (bingoCard[targetid].ticked == true) {
-        bingoCard[targetid].element.classList.remove("ticked")
-        bingoCard[targetid].ticked = false
+    if (bingoSessionData[targetid].ticked == true) {
+        bingoElements[targetid].classList.remove("ticked")
+        bingoSessionData[targetid].ticked = false
     } else {
-        bingoCard[targetid].element.classList.add("ticked")
-        bingoCard[targetid].ticked = true
+        bingoElements[targetid].classList.add("ticked")
+        bingoSessionData[targetid].ticked = true
     }
     
-    localStorage.setItem("bingo", JSON.stringify(bingoCard))
+    localStorage.setItem("bingo", JSON.stringify(bingoSessionData))
 }
 
 function bingoRender() {
     if (bingoContainer.firstChild != null) {
         for (let i = 0; i < bingoContainer.childNodes.length; i++) {
-            bingoContainer[i].innerHtml = bingoCard[i].text;
+            bingoContainer[i].innerHtml = bingoSessionData[i].text;
         }
     }
     else {
         for (let i = 0; i < 25; i++) {
-            bingoContainer.appendChild(bingoCard[i].element)
+            bingoContainer.appendChild(bingoElements[i])
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# AV Bingo
+## Bingo löytyy osoiteesta: https://villevilli.github.io/av-bingo/
+
+Jos haluat contrituboida niin tee pull request tai jotain lmao
+
+Mit lisenssi kait lol se on ihan jees tehkää mitä se antaa
